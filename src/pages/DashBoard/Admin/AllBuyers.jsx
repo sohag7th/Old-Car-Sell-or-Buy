d2244ad99a5f1715b95dad75f00b0e87be7fc93d
Sohag <sohaghossain7th@gmail.com>
2022-11-28 16:11:28 +0600

    All Buyer data show

--- src/pages/DashBoard/Admin/AllBuyers.jsx
+++ src/pages/DashBoard/Admin/AllBuyers.jsx
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../../../shared/Loading';
+import DataRow from './DataRow';
 
 const AllBuyers = () => {
+    const [deleteProduct, setDeleteProduct] = useState(false);
+
+    const { data: sellers, isLoading , refetch } = useQuery('sellers', () => fetch(`http://localhost:5000/buyers`,{
+            method: 'GET',
+            headers: {
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        }
+    ).then(res => res.json()), );
+    if (isLoading) {
+        return <Loading></Loading>
+    }
     return (
-        <div>
-            
+        <div className='ml-20'>
+        <div className="overflow-x-auto">
+            <table className="table w-full">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Status</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        sellers?.map((seller, index) => <DataRow
+                            key={seller._id}
+                            seller={seller}
+                            refetch={refetch}
+                            index={index}
+                            setDeleteProduct={setDeleteProduct}
+                        ></DataRow>)
+                    }
+
+
+                </tbody>
+            </table>
+
+            {/* {
+                deleteProduct && <DeleteConfirmationModal
+                    deleteProduct={deleteProduct}
+                    setDeleteProduct={setDeleteProduct}
+                    url={url}
+                    refetch={refetch}
+                >
+                </DeleteConfirmationModal>
+            } */}
         </div>
+    </div>
     );
 };
+
 
 export default AllBuyers;
